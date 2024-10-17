8c1ed4dab9c612dcb1ad3db6320b21a9840cdd90
sunken-bishop <roryhealy2005@gmail.com>
2024-10-17 14:31:34 +0100

    ghilgulul

--- global.gd
+++ global.gd
@@ -5,7 +5,7 @@ var oldIncrement = 0
 var damage = 1
 var inventoryDisplays = {"longsword":["res://sword.png", "weapon"], "grass":["res://icon.svg", "consumable"], "bomb":["res://icon.svg", "consumable"], "flamberge":["res://icon.svg", "weapon"],
 							"parryingdagger":["res://sword.png", "leftHandWeapon"]}
-var weapons = {"longsword": [preload("res://longsword.tscn"), "Hello lol"], "flamberge": [preload("res://flamberge.tscn"), "Yurp"]}
+var weapons = {"longsword": [preload("res://weapons/longsword.tscn"), "Hello lol"], "flamberge": [preload("res://weapons/flamberge.tscn"), "Yurp"]}
 var leftHandWeapons = {"shield": [preload("res://shield.tscn"), "Common shield for blocking and parrying blades."], "lamp": [preload("res://misc/lamp.tscn"), "Lamp used to illuminate the dark."]}
 var consumables = {"grass":[preload("res://worldAssets/grass.tscn"), "consume"], "bomb":[0,50, "throw"]}
 var stackables = ["ammo", "healing"]
